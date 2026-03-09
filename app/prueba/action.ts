1e64a25295ff4c8f2b36b1dfa33810ed8231db21
augustoterrera <augustoterrera@gmail.com>
2026-03-09 01:30:08 -0300

    feat: eliminar archivos de configuración y acciones no utilizadas

--- app/prueba/action.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "@/lib/db";
-
-export async function getInvoices() {
-    const data = await db`SELECT * FROM facturas WHERE id_factura = ${1562070}`;
-    console.log("data:", data);
-}
